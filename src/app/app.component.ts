6d2180327c5bd69182a91648b5e7ec9dc1e9ed34
Matt Clyde <mclyde@nextcenturymeters.com>
2021-05-15 22:07:19 -0600

    working on mobile display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, merge, Observable, BehaviorSubject, Subject, combineLatest } from 'rxjs';
+import { distinctUntilChanged, map, takeUntil } from 'rxjs/operators';
 import * as tallImagesMeta from '../assets/tall-images.json';
 import * as wideImagesMeta from '../assets/wide-images.json';
 const WIDE_IMAGES = (wideImagesMeta as any).default;
@@ -7,10 +9,66 @@ const TALL_IMAGES = (tallImagesMeta as any).default;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
-  bannerImageUrl = WIDE_IMAGES[Math.floor(Math.random() * WIDE_IMAGES.length)].url;
+  bannerImageUrl: Observable<string>;
+  showFullHeightHeader: Observable<boolean>;
+
+
+  private _bannerImageUrl = new BehaviorSubject<string>('');
+  private _destroy = new Subject();
+
+  constructor() {
+    this.bannerImageUrl = this._bannerImageUrl.asObservable();
+  }
+
+  ngOnInit(): void {
+    const isMobileSize = merge(
+      [window.innerWidth],
+      fromEvent(window, 'resize').pipe(
+        map((event: any) => event.target.innerWidth),
+      ),
+    ).pipe(
+      takeUntil(this._destroy),
+      map((width) => width < 500),
+      distinctUntilChanged(),
+    );
+
+    isMobileSize.subscribe((val) => {
+      if (val) {
+        this._bannerImageUrl.next(this.getRandomImage(TALL_IMAGES));
+      } else {
+        this._bannerImageUrl.next(this.getRandomImage(WIDE_IMAGES));
+      }
+    });
+
+    const scrollAtTopOfPage =
+      merge(
+        [window.scrollY],
+        fromEvent(window, 'scroll').pipe(map((event) => (event.target as any).scrollingElement.scrollTop)),
+      ).pipe(
+        takeUntil(this._destroy),
+        map((scrollPosition) => scrollPosition < 100),
+        distinctUntilChanged(),
+      );
+
+    this.showFullHeightHeader = combineLatest([isMobileSize, scrollAtTopOfPage]).pipe(
+      map(([mobileSize, atTop]) => atTop && !mobileSize),
+      distinctUntilChanged(),
+    );
+
+    this.showFullHeightHeader.subscribe((val) => console.log('%c Show Full Height Header', 'color: red', val));
+  }
+
+  ngOnDestroy(): void {
+    this._destroy.next();
+  }
+
+  private getRandomImage(images: Array<{ url: string }>): string {
+    return images[Math.floor(Math.random() * images.length)].url;
+  }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppHeaderComponent implements OnInit {
+
+  @Input() fullHeight = true;
 
   constructor() { }
 
